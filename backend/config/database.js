96278d039fc19ee2d0de0920e083046da1386c91
Shehab Shawqi Abduh Al-Dhahra <Shehabshawgi@gmail.com>
2026-08-05 10:22:49 +0300

    removing the default DB_PASSWORD

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(
   process.env.DB_NAME || 'safwa_db',
   process.env.DB_USER || 'root',
-  process.env.DB_PASSWORD || '62130137',
+  process.env.DB_PASSWORD || '',
   {
     host: process.env.DB_HOST || 'localhost',
     dialect: 'mysql',
